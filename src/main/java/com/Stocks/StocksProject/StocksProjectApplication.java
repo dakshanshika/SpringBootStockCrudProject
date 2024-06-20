2323307ef326ae6a6078d6d417cc251a41a9d2b0
Anshika <dakshanshika1995@gmail.com>
2024-06-21 01:08:19 +0530

    Comments added in main class

--- src/main/java/com/Stocks/StocksProject/StocksProjectApplication.java
+++ src/main/java/com/Stocks/StocksProject/StocksProjectApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StocksProjectApplication {
 
+
+//	this is my main class
 	public static void main(String[] args) {
 		SpringApplication.run(StocksProjectApplication.class, args);
 	}
